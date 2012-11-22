4faa313761a41cd875171a9447a32e1b5e03c25d
Yoan Blanc <yoan@dosimple.ch>
2012-11-22 22:50:27 +0100

    Some early measures.

---
+++ measures.sh
@@ -0,0 +1,8 @@
+IMAGES="100x100@24.bmp 300x300@24.bmp 1680x1050@24.bmp 8000x8000@24.bmp"
+TMP=_.bmp
+
+cd $1 && make serial && cd ..
+for img in $IMAGES; do
+    $1/serial img/$img $TMP
+    rm -f $TMP
+done
