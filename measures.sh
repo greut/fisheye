91da70772defed61b94fe233578e5e3f3e2eba57
Yoan Blanc <yoan@dosimple.ch>
2012-11-23 21:08:03 +0100

    Clean up the output.

--- measures.sh
+++ measures.sh
@@ -1,7 +1,7 @@
 IMAGES="100x100@24.bmp 300x300@24.bmp 1680x1050@24.bmp 8000x8000@24.bmp"
 TMP=_.bmp
 
-cd $1 && make serial && cd ..
+cd $1 && make serial > /dev/null && cd ..
 for img in $IMAGES; do
     $1/serial img/$img $TMP
     rm -f $TMP
