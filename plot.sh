e9c89d70b4c01e7e913e607fd6eef3101647e95e
Yoan Blanc <yoan@dosimple.ch>
2012-12-18 11:08:34 +0100

    Latest measures on BC machine

--- plot.sh
+++ plot.sh
@@ -39,10 +39,39 @@ gnuplot <<EOF
         "$data/serial3.tsv" u (\$2/$f):(\$5+\$6) every ::2 title "serial 3" with linespoints, \\
         "$data/serial4.tsv" u (\$2/$f):(\$5+\$6) every ::2 title "serial 4" with linespoints, \\
         "$data/serial5.tsv" u (\$2/$f):(\$5+\$6) every ::2 title "serial 5" with linespoints, \\
-        "$data/serial6.tsv" u (\$2/$f):(\$5+\$6) every ::2 title "serial 6" with linespoints, \\
+        "$data/serial6.tsv" u (\$2/$f):(\$5+\$6) every ::2 title "serial 6" with linespoints
+EOF
+
+gnuplot <<EOF
+    set terminal png
+    set output "plots/areatime_compute_linear_with_omp.png"
+    set title "Area vs Computational Time"
+    set xlabel "area [$f * pixel^2]"
+    set ylabel "time [sec]"
+    set key left top
+    set datafile separator "\t"
+    plot "$data/serial0.tsv" u (\$2/$f):5 title "serial 0" with linespoints, \\
+        "$data/serial5.tsv" u (\$2/$f):(\$5+\$6) title "serial 5" with linespoints, \\
+        "$data/openmp0.tsv" u (\$2/$f):(\$5+\$6) title "openmp 0 ($cores)" with linespoints, \\
+        "$data/openmp1.tsv" u (\$2/$f):(\$5+\$6) title "openmp 1 ($cores)" with linespoints
+EOF
+
+gnuplot <<EOF
+    set terminal png
+    set output "plots/areatime_compute_log_omp.png"
+    set title "Area vs Computational Time in Logscale"
+    set xlabel "area [log($f * pixel^2)]"
+    set ylabel "time [log(sec)]"
+    set logscale xy
+    set key left top
+    set datafile separator "\t"
+    plot "$data/serial0.tsv" u (\$2/$f):5 every ::2 title "serial 0" with linespoints, \\
+        "$data/serial5.tsv" u (\$2/$f):(\$5+\$6) every ::2 title "serial 5" with linespoints, \\
         "$data/openmp0.tsv" u (\$2/$f):(\$5+\$6) every ::2 title "openmp 0 ($cores)" with linespoints, \\
         "$data/openmp1.tsv" u (\$2/$f):(\$5+\$6) every ::2 title "openmp 1 ($cores)" with linespoints
 EOF
+
+
 
 gnuplot <<EOF
     set terminal png
